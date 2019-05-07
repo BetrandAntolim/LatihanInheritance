d44f3425c3398093ecfe5627e12c9205f6cd89b8
BetrandAntolim <antontheamor123@gmail.com>
2019-05-07 14:39:54 +0700

    first commit

---
+++ LatihanInheritance/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    public class Student
+    { 
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string StudentId { get; set; }
+        public string Email { get; set; }
+
+        public Student(string name, int age, string studentid, string email)
+        {
+            Name = name;
+            Age = age;
+            StudentId = studentid;
+            Email = email;
+        }
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The Persen Has Name : {0} and Age : {1}", Name, Age);
+        }
+
+    }
+}
---
+++ LatihanInheritance/Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+
+        public Teacher(string name, int age, string teacherid, string subject)
+        {
+            Name = name;
+            Age = age;
+            TeacherId = teacherid;
+            Subject = subject;
+        }
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The Persen Has Name : {0} and Age : {1}", Name, Age);
+        }
+    }
+}
